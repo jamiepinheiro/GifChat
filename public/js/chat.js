c6464b44f60e1f029b49c86e720d6b02692ce74f
Jamie Pinheiro <jamiepinheiro@gmail.com>
2018-01-04 20:15:32 -0500

    Added scrollable message area

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,25 +11,38 @@ var doneTypingInterval = 1000;
 var $input = $('#gif-search');
 
 $input.on('keyup', function () {
-  clearTimeout(typingTimer);
-  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
 });
 
 $input.on('keydown', function () {
-  clearTimeout(typingTimer);
+    clearTimeout(typingTimer);
+});
+
+$input.change(function () {
+    clearTimeout(typingTimer);
+    doneTyping()
 });
 
 function doneTyping () {
-  var search = $input.val();
-  $.get('/gifs?search=' + search, function (data) {
-      for (var i = 0; i < 4; i++) {
-          var $img = $(`#gif-${i}`);
-          $img.attr('src', data[i].url)
-          $img.height($img.height());
-          $img.width($img.height());
-      }
-  });
-}
+    //MOVE INTO LOAD MESSAGE LOGIC
+    var $messageArea = $('#message-area');
+    $messageArea.css('max-height', ($(window).height() - $('#gif-search-area').height()) + 'px');
+    $messageArea.scrollTop = $messageArea.scrollHeight;
 
+    var search = $input.val();
+
+    if (search.length >= 2) {
+        $.get('/gifs?search=' + search, function (data) {
+          for (var i = 0; i < 4; i++) {
+              var $img = $(`#gif-${i}`);
+              $img.attr('src', data[i].url)
+              $img.height($img.height());
+              $img.width($img.height());
+          }
+        });
+    }
+
+}
 
 console.log($('#gif-search').val());
